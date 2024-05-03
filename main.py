cbb922ca9b1109c0a731ea20b2436f9aee98b83f
Hugo Sanz González <80487270+hsanzg@users.noreply.github.com>
2024-05-03 23:23:36 +0200

    Progress

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ MONGO_CONN_STRING = '127.0.0.1:27017'
 print(f'connecting to {MONGO_CONN_STRING}')
 client = MongoClient(MONGO_CONN_STRING)
 
-# Our database is called "workplace"
+# Our database and collection are both called "workplace"
 db = client.metrics
 collection = db.create_collection('metrics', timeseries={
   'timeField': 'timestamp',
@@ -17,11 +17,11 @@ collection = db.create_collection('metrics', timeseries={
 
 # warning: this method modifies the passed `metrics` object
 def upload(metrics):
-  time = datetime.now()
+  time = datetime.now() # todo: utcnow?
   count = len(metrics.keys())
   print(f'[{time}] uploading {count} metrics')
   metrics['metadata'] = {}
-  metrics['timestamp'] = time # todo: utcnow?
+  metrics['timestamp'] = time
   collection.insert_one(metrics)
 
 upload({
