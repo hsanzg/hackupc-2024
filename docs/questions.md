0d7e0b30bff988f2560bdc508004eb47fb1b3c17
Hugo Sanz González <80487270+hsanzg@users.noreply.github.com>
2024-05-03 23:57:23 +0200

    Progress

---
+++ docs/questions.md
@@ -0,0 +1,2 @@
+- Difference between big and small sound
+- Difference between two temperature sensors, accuracy? Did we correctly identify them?
--- main.py
+++ main.py
@@ -3,17 +3,22 @@ import os
 import datetime
 
 MONGO_CONN_STRING = '127.0.0.1:27017'
+COLLECTION_NAME = 'metrics'
 
 print(f'connecting to {MONGO_CONN_STRING}')
 client = MongoClient(MONGO_CONN_STRING)
 
 # Our database and collection are both called "workplace"
 db = client.metrics
-collection = db.create_collection('metrics', timeseries={
-  'timeField': 'timestamp',
-  'metaField': 'metadata',
-  'granularity': 'seconds'
-})
+
+if COLLECTION_NAME not in db.list_collection_names():
+  collection = db.create_collection('metrics', timeseries={
+    'timeField': 'timestamp',
+    'metaField': 'metadata',
+    'granularity': 'seconds'
+  })
+else:
+  collection = db.metrics
 
 # warning: this method modifies the passed `metrics` object
 def upload(metrics):
