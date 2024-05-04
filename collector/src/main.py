d5e8caf575a1a99b1a96574a35dae712f8203de8
Hugo Sanz González <80487270+hsanzg@users.noreply.github.com>
2024-05-04 22:47:42 +0200

    More invented stats

--- collector/src/main.py
+++ collector/src/main.py
@@ -36,13 +36,19 @@ def get_esp_port():
 SD_DIFF_FACTOR = 1e-2
 BIAS_FACTOR = 1e-4
 
+temp_mean = 22.2
+hum_mean = 44
+temp_people_factor = .05
+
 co2_mean = 700 # ppm
-co2_std_dev = 100
+co2_std_dev = 200
+co2_people_factor = 10
 co2 = rnd.normal(co2_mean, co2_std_dev)
 
 sound_mean = 45 # dB
 sound_std_dev = 4
 sound_peak_p = 1 / (30 * 60)
+sound_people_factor = .5
 sound = rnd.normal(sound_mean, sound_std_dev)
 
 people_mean = 20
@@ -84,19 +90,30 @@ if __name__ == '__main__':
       sound += rnd.normal(2 * sound_std_dev, sound_std_dev / 2)
     people = gen_next(people_mean, people_std_dev, people)
     # Add extra noise scale to people.
-    people += people_second_scale * np.sin(people_theta)
+    people += people_second_scale * np.sin(people_theta) + np.normal(0, people_std_dev / 2)
     people_theta += people_theta_delta
     people = max(np.round(people), 0) # cap it, and round it
+    rel_people_diff = (people - people_mean) / people_mean
+    sound += rel_people_diff * sound_people_factor
+    temp += rel_people_diff * temp_people_factor
+    co2 += rel_people_diff * co2_people_factor
+
+    # Compute "magical" score
+    score = .5 * np.abs(temp - temp_mean) +\
+            .1 * np.abs(humidity - hum_mean) +\
+            .2 * np.abs(sound - sound_mean) +\
+            .2 * np.abs(co2 - co2_mean)
 
     if math.isnan(temp) or not (-20 < temp < 50) or not (0 < humidity < 100):
       print(f'failed checksum or invalid data (temp={temp}, hum={humidity}, sound={sound}); skipping')
     else:
-      print(f'uploading temp={temp}ºC, hum={humidity}%, sound={sound}db, co2={co2}ppm, people={people}')
+      print(f'uploading temp={temp}ºC, hum={humidity}%, sound={sound}db, co2={co2}ppm, people={people}, score={score}')
       if SHOULD_UPLOAD:
         p = Point('workplace')\
               .field('temperature', temp)\
               .field('humidity', humidity)\
               .field('co2', co2)\
               .field('sound', sound)\
-              .field('people', people)
+              .field('people', people)\
+              .field('score', score)
         upload(write_api, p)
